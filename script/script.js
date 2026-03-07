d56955e0e2c6181a9ccfea03f445f64a69197d94
sabbir053 <sabbirhossain0900@gmail.com>
2026-03-08 01:59:41 +0600

    Complete Loading System

--- script/script.js
+++ script/script.js
@@ -3,10 +3,21 @@ const buttons = document.querySelectorAll(".filter-btn");
 
 let allIssuesData = [];
 
+// loader show hide 
+
+function showLoader() {
+    document.getElementById("loader").classList.remove("hidden");
+}
+
+function hideLoader() {
+    document.getElementById("loader").classList.add("hidden");
+}
+
 // =====================
 // Load Issues
 // =====================
 async function loadIssues() {
+    showLoader()
 
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
@@ -14,15 +25,19 @@ async function loadIssues() {
     allIssuesData = data.data;
 
     renderIssues(allIssuesData);
+
+    hideLoader()
 }
 
 loadIssues();
+showLoader()
 
 
 // =====================
 // Render Issues
 // =====================
 function renderIssues(issues) {
+    showLoader()
 
     allCards.innerHTML = "";
 
@@ -36,8 +51,8 @@ function renderIssues(issues) {
         <div onclick="modalDetails(${issue.id})" 
         class="card bg-white p-3 space-y-3 shadow-sm 
         ${issue.priority == "high" || issue.priority == "medium"
-            ? "border-t-3 border-green-500"
-            : "border-t-3 border-purple-500"}">
+                ? "border-t-3 border-green-500"
+                : "border-t-3 border-purple-500"}">
 
             <div class="flex justify-between items-center">
 
@@ -47,10 +62,10 @@ function renderIssues(issues) {
 
                 <p class="px-4 py-2 rounded-full 
                 ${issue.priority === "high"
-                    ? "bg-red-100 text-red-500"
-                    : issue.priority === "medium"
-                        ? "bg-yellow-100 text-yellow-500"
-                        : "bg-gray-100 text-gray-500"}">
+                ? "bg-red-100 text-red-500"
+                : issue.priority === "medium"
+                    ? "bg-yellow-100 text-yellow-500"
+                    : "bg-gray-100 text-gray-500"}">
 
                 ${issue.priority}
                 </p>
@@ -65,39 +80,39 @@ function renderIssues(issues) {
 
                 ${issue.labels.map(label => {
 
-                    if (label[0] === "b") {
-                        return `
+                        if (label[0] === "b") {
+                            return `
                         <div class="flex gap-2 items-center px-2 py-1 bg-red-50 rounded">
                             <i class="fa-solid fa-bug text-red-500"></i>
                             <p class="text-red-500">${label}</p>
                         </div>`;
-                    }
+                        }
 
-                    else if (label[0] === "h") {
-                        return `
+                        else if (label[0] === "h") {
+                            return `
                         <div class="flex gap-2 items-center px-2 py-1 bg-green-50 rounded">
                             <i class="fa-solid fa-helicopter-symbol text-green-500"></i>
                             <p class="text-green-500">${label}</p>
                         </div>`;
-                    }
+                        }
 
-                    else if (label[0] === "e") {
-                        return `
+                        else if (label[0] === "e") {
+                            return `
                         <div class="flex gap-2 items-center px-2 py-1 bg-yellow-50 rounded">
                             <i class="fa-regular fa-star text-yellow-500"></i>
                             <p class="text-yellow-500">${label}</p>
                         </div>`;
-                    }
+                        }
 
-                    else {
-                        return `
+                        else {
+                            return `
                         <div class="flex gap-2 items-center px-2 py-1 bg-orange-50 rounded">
                             <i class="fa-regular fa-star text-orange-500"></i>
                             <p class="text-orange-500">${label}</p>
                         </div>`;
-                    }
+                        }
 
-                }).join("")}
+                    }).join("")}
 
             </div>
 
@@ -114,6 +129,7 @@ function renderIssues(issues) {
         allCards.appendChild(div);
 
     });
+    hideLoader()
 
 }
 
@@ -122,6 +138,7 @@ function renderIssues(issues) {
 // Modal Details
 // =====================
 async function modalDetails(id) {
+    showLoader()
 
     const detailsBox = document.getElementById("details-container");
 
@@ -135,7 +152,7 @@ async function modalDetails(id) {
     <h2 class="text-2xl font-bold">${issue.title}</h2>
 
     <div class="flex gap-3 py-3 items-center">
-        <p class="px-4 py-2 rounded-md ${issue.status == "all" || issue.status == "open"? 'bg-green-500 text-white': ' bg-red-500 text-white'}">${issue.status}</p>
+        <p class="px-4 py-2 rounded-md ${issue.status == "all" || issue.status == "open" ? 'bg-green-500 text-white' : ' bg-red-500 text-white'}">${issue.status}</p>
         <p class="text-gray-500">Open by: ${issue.author}</p>
         <p class="text-gray-500">${issue.createdAt}</p>
     </div>
@@ -158,6 +175,7 @@ async function modalDetails(id) {
     `;
 
     document.getElementById("modal_details").showModal();
+    hideLoader()
 }
 
 
@@ -166,24 +184,21 @@ async function modalDetails(id) {
 // =====================
 buttons.forEach(button => {
 
-    button.addEventListener("click", function () {
+    button.addEventListener("click", async function () {
 
-        buttons.forEach(btn => {
-            btn.classList.remove("bg-primary", "text-white");
-        });
-
+        buttons.forEach(btn => btn.classList.remove("bg-primary", "text-white"));
         this.classList.add("bg-primary", "text-white");
 
         const filter = this.dataset.filter;
 
-        if (filter === "all") {
-            renderIssues(allIssuesData);
-        }
+        showLoader();
+        allCards.innerHTML = "";
 
-        else {
-            const filtered = allIssuesData.filter(issue => issue.status === filter);
-            renderIssues(filtered);
-        }
+        let filtered = (filter === "all") ? allIssuesData : allIssuesData.filter(issue => issue.status === filter);
+
+        renderIssues(filtered);
+
+        hideLoader();
 
     });
 
@@ -198,6 +213,7 @@ buttons.forEach(button => {
 
 
 document.getElementById("btn-search").addEventListener("keyup", function () {
+    showLoader()
 
     const value = this.value.toLowerCase();
     const searchValue = value.trim()
@@ -207,6 +223,9 @@ document.getElementById("btn-search").addEventListener("keyup", function () {
         issue.description.toLowerCase().includes(searchValue)
     );
 
+    console.log(filteredIssues)
+
     renderIssues(filteredIssues);
+    hideLoader()
 
 });
